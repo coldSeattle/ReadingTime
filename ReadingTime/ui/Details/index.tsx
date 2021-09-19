3304d29e309c27cdf7f2af2b836ca4c318342faa
David Movsesyan <coldseattle@mail.ru>
2021-09-19 22:44:03 +0300

    configure app navigation

---
+++ ReadingTime/ui/Details/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const DetailsScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text style={{color: '#000'}}>Details Screen</Text>
+    </View>
+  );
+};
---
+++ ReadingTime/ui/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const HomeScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text style={{color: '#000'}}>Details Screen</Text>
+    </View>
+  );
+};
