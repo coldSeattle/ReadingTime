eac5b4199e11a7b5c7e3788c911df70f3e8c7fb1
David Movsesyan <coldseattle@mail.ru>
2021-09-22 22:52:30 +0300

    add valid fetch books by q endpoint

--- ReadingTime/services/api.ts
+++ ReadingTime/services/api.ts
@@ -6,11 +6,12 @@ export const googleApi = `https://www.googleapis.com`;
 export const auth = `/auth`;
 export const books = `/books`;
 export const volumns = 'volumns';
+export const version = 'v1';
 
 export default {
   getSearchedBooks(resource: string, params: any) {
     const {query} = params.data;
-    const url = `${resource}/?q=${query}`;
+    const url = `${resource}/${books}/${version}/${volumns}?q=${query}`;
     return fetch(url).then(response => response.json());
   },
 };
